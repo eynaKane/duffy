20fde62fad203c30408b2991afc2339710b3dcc1
ekane <ekane@optoro.com>
2016-08-07 14:52:01 -0400

    Better loop

--- duffy.rb
+++ duffy.rb
@@ -1,18 +1,13 @@
-print "What's on your mind, Duffy? "
-user_input = gets.chomp
-
-until user_input == 'exit'
+loop do
+  print "What's on your mind, Duffy? "
+  user_input = gets.chomp
+  break if user_input == 'exit'
   s = { (/s/) => ('th'), (/S/) => ('Th'),
         (/ce/) => ('the'), (/Ce/) => ('The'),
         (/ci/) => ('thi'), (/Ci/) => ('Thi'),
         (/cy/) => ('thy'), (/Cy/) => ('Thy') }
 
-  s.each do |x, y|
-    user_input.gsub!(x, y)
-  end
+  s.each { |x, y| user_input.gsub!(x, y) }
 
   puts "Duffy says \"#{user_input}\""
-
-  print "What's on your mind, Duffy? "
-  user_input = gets.chomp
 end
