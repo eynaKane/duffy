a18b31577b0546e9573a5fab9bdde8b7ab55a224
ekane <ekane@optoro.com>
2016-08-07 16:05:26 -0400

    Thmarter

--- duffy.rb
+++ duffy.rb
@@ -1,8 +1,8 @@
 loop do
   print "What's on your mind, Duffy? "
   user_input = gets.chomp
-  break if user_input == 'exit'
-  s = { (/s/) => ('th'), (/S/) => ('Th'),
+  break if user_input =~ /exit/i
+  s = { (/t?s+/) => ('th'), (/t?S+/) => ('Th'),
         (/ce/) => ('the'), (/Ce/) => ('The'),
         (/ci/) => ('thi'), (/Ci/) => ('Thi'),
         (/cy/) => ('thy'), (/Cy/) => ('Thy') }
