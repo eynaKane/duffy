36932692eeab50a3ca66e647060408cabd148d89
Eyna Kane <eyna.kane@gmail.com>
2018-11-03 20:32:50 -0400

    Fix spacing

--- duffy.rb
+++ duffy.rb
@@ -5,10 +5,10 @@ loop do
   print "What's on your mind, Duffy? "
 
   begin
-	  user_input = Timeout::timeout(3) { gets.chomp }
-	rescue
-		break
-	end
+  	user_input = Timeout::timeout(3) { gets.chomp }
+  rescue
+  	break
+  end
 
   s = { (/t?s+/) => ('th'), (/t?S+/) => ('Th'),
         (/ce/) => ('the'), (/Ce/) => ('The'),
